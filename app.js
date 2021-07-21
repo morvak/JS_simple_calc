639fee9de350af44893a2a209aebdb70799a9871
morvak <markov.roman.forwork@gmail.com>
2021-07-21 09:19:20 +0300

    operation order works fine
    first step is *and /
    second step +and -

--- app.js
+++ app.js
@@ -1,43 +1,8 @@
-let expression = "2+(11-(34+66-2)-22*5)/(4+2)";
-
-console.log("separate by `(` or `)`");
-
-let exprWithoutBrackets = expression.split(/[\(|\)]/);
-exprWithoutBrackets = exprWithoutBrackets.filter(function (el) {
-    if(el) {return el};
-});
-console.log(exprWithoutBrackets);
-
 console.log("========================================");
-
-console.log("separate by all spechial characters");
-let expOnlyNumbers = expression.split(/[\+|\-|\*|\/|\(|\)]/);
-expOnlyNumbers = expOnlyNumbers.filter(function (el) {
-    if(el) { return el };
-});
-let numbers = [];
-expOnlyNumbers.forEach(element => {
-    if (element){
-        numbers.push(parseInt(element));
-    }
-});
-console.log(numbers);
-
-console.log("========================================");
-
-console.log("only operations");
-let operations = expression.split(/[\d+]/);
-operations = operations.filter(function (el) {
-    if(el) {return el};
-});
-console.log(operations);
-
-console.log("========================================");
-expression = "2+11-34+66-2-22*5/4+2";
+expression = "100-20/4*2-2*3+6";
 console.log(expression);
 console.log("========================================");
 let arrExpr = [];
-expression = "2+1-34+66-2-22*5/4+2";
 const regex = new RegExp(/\+|\-|\*|\//);
 let exprLenght = expression.length;
 let start = 0;
@@ -54,21 +19,18 @@ for (let i = 0; i < exprLenght+1; i++) {
 console.log(arrExpr);
 console.log(arrExpr.length);
 
-function oneStep (arrExpr) {
+function stepOne (arrExpr) {
     let res = 0;
     for (let j = 0; j < arrExpr.length; j++) {
         if (j%2!=0) {
-            console.log(j);
             if (arrExpr[j] === "*") {
                 res = (arrExpr[j-1] * arrExpr[j+1]);
-                arrExpr = arrExpr.filter(item => item !== arrExpr[j]);
-                arrExpr = arrExpr.filter(item => item !== arrExpr[j]);
+                arrExpr.splice(j, 2);
                 arrExpr[j-1] = res;
                 break;
             } else if (arrExpr[j] === "/") {
                 res = (arrExpr[j-1] / arrExpr[j+1]);
-                arrExpr = arrExpr.filter(item => item !== arrExpr[j]);
-                arrExpr = arrExpr.filter(item => item !== arrExpr[j]);
+                arrExpr.splice(j, 2);
                 arrExpr[j-1] = res;
                 break;
             }
@@ -78,27 +40,18 @@ function oneStep (arrExpr) {
     return (arrExpr);
 };
 
-function twoStep (arrExpr) {
+function stepTwo (arrExpr) {
     let res = 0;
     for (let j = 0; j < arrExpr.length; j++) {
         if (j%2!=0) {
-            console.log(j);
             if (arrExpr[j] === "+") {
                 res = (arrExpr[j-1] + arrExpr[j+1]);
-                console.log(arrExpr);
-                arrExpr = arrExpr.filter(item => item !== arrExpr[j]);
-                console.log(arrExpr);
-                arrExpr = arrExpr.filter(item => item !== arrExpr[j]);
-                console.log(arrExpr);
+                arrExpr.splice(j, 2);
                 arrExpr[j-1] = res;
                 break;
             } else if (arrExpr[j] === "-") {
                 res = (arrExpr[j-1] - arrExpr[j+1]);
-                console.log(arrExpr);
-                arrExpr = arrExpr.filter(item => item !== arrExpr[j]);
-                console.log(arrExpr);
-                arrExpr = arrExpr.filter(item => item !== arrExpr[j]);
-                console.log(arrExpr);
+                arrExpr.splice(j, 2);
                 arrExpr[j-1] = res;
                 break;
             }
@@ -109,19 +62,13 @@ function twoStep (arrExpr) {
 };
 
 while (true){
-    if(arrExpr.find(item => item === "*")){
-        arrExpr = oneStep(arrExpr);
-    } else if (arrExpr.find(item => item === "/")) {
-        arrExpr = oneStep(arrExpr);
-    } else if (arrExpr.find(item => item === "-")){
-        arrExpr = twoStep(arrExpr);
-    } else if ((arrExpr.find(item => item === "+"))) {
-        arrExpr = twoStep(arrExpr);
+    if(arrExpr.find(item => item === "*") || arrExpr.find(item => item === "/")){
+        arrExpr = stepOne(arrExpr);
+        console.log(arrExpr);
+    } else if (arrExpr.find(item => item === "-") || (arrExpr.find(item => item === "+"))){
+        arrExpr = stepTwo(arrExpr);
+        console.log(arrExpr);
     } else {
         break;
     }
-    console.log(arrExpr);
-
 }
-
-console.log(arrExpr);
